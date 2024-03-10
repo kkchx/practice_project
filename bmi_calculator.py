822b023d2ddf5a035abff0c4cba041a73f0a7318
cryptoracing <101093104+cryptoracing@users.noreply.github.com>
2024-03-10 13:20:47 +0700

    suggestions for solving the problem

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,8 +1,20 @@
+#let this function only calculate bmi
 def cal_bmi(weight,height):
     result = round(weight/(height/100)**2,1)
-    if result < 18.5
+    # if result < 18.5
     return result
 
+# create another function to interpret the results
+def results_interpretations(bmi):
+    if bmi < 18.5:
+        return "You are underweight"
+    elif bmi <24.9:
+        return ""
+    ...
+
+
+# improve the exception handling with try/except blocks
 weight = int(input("Enter weight: "))
 height = int(input("Enter the height: "))
-print("Your bmi is",cal_bmi(weight,height))
+bmi = cal_bmi(weight,height)
+print(f"Your bmi is {bmi} and {results_interpretations(bmi)}")
