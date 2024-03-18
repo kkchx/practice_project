b9fdec8ff60d2926e32f7bd83a9379d13e97125d
kkchx <std46375@sriyapai.ac.th>
2024-03-18 15:23:29 +0700

    fix exception handler bmi_calculator.py.md

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,24 +1,29 @@
-try:
-    def cal_bmi(weight,height):
+def cal_bmi(weight,height):
+    try:
         result = round(weight/(height/100)**2,1)
+    except ZeroDivisionError:
+        print("Height can't be zero")
+        return -1
+    else:
         return result
 
-    def ana_li_ze():
-        result = cal_bmi(weight,height) # this is extra
-        if result < 18.5:
-            print("you're underweight")
-        elif result < 24.9:
-            print("you're healthy")
-        elif result < 29.9:
-            print("you're overweight")
-        else:
-            print("you're obese")
+def ana_li_ze(bmi):
+    if bmi < 18.5:
+        return "you're underweight"
+    elif bmi < 24.9:
+        return "you're healthy"
+    elif bmi < 29.9:
+        return "you're overweight"
+    else:
+        return "you're obese"
 
-
+try:
     weight = int(input("Enter weight: "))
     height = int(input("Enter the height: "))
-    # create a global BMI var
-    bmi = cal_bmi(weight,height)
-    print("Your bmi is",bmi,ana_li_ze())
 except ValueError:
     print("Please enter number")
+else:
+    # create a global BMI var
+    bmi = cal_bmi(weight,height)
+    if bmi !=-1:
+        print(f"Your bmi is {bmi}, {ana_li_ze(bmi)}")
