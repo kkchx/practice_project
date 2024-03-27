82f18cf73c15280d1eed7a5a12a3a0b2ad146dba
cryptoracing <101093104+cryptoracing@users.noreply.github.com>
2024-03-27 16:56:34 +0700

    created a test and edited the code

--- TicTacToe/Game.py
+++ TicTacToe/Game.py
@@ -23,8 +23,50 @@ def make_move(player, board, x, y):
         return False
 
 
-board = [[" " for i in range(3)] for j in range(3)]
-player = "x"
-draw_board(board)
-x, y = ask_move(player)
-make_move(player,board,x,y)
+def ask_and_make_move(player, board):
+    while True:
+        x, y = ask_move(player)
+        if make_move(player, board, x, y):
+            break
+
+def check_win(player, board):
+    # check i wins
+    for i in range(3):
+        if board[i]==[player,player,player]:
+            return True
+        if board[0][i] == player and board[1][i] == player and board[2][i] == player:
+            return True
+
+
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    return False
+
+
+def check_tie(board):
+    return False
+
+
+def tic_tac_toe():
+    while True:
+        # Create a list of lists, filled with spaces " "
+        board = [[" " for i in range(3)] for j in range(3)]
+        player = "X"
+        while True:
+            draw_board(board)
+            ask_and_make_move(player, board)
+            if check_win(player, board):
+                print(f"{player} wins!")
+                break
+            if check_tie(board):
+                print("It's a tie!")
+                break
+            player = "O" if player == "X" else "X"
+        restart = input("Do you want to play again? (y/n) ")
+        if restart.lower() != "y":
+            break
+
+if __name__ == "__main__":
+    tic_tac_toe()
